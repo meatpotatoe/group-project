ba22b6a526a17ec137b70ad67734a56ed75a46bb
vingspann <iblvinathgcldlv@gmail.com>
2015-03-17 13:52:43 -0700

    book.h over-write

--- book.h
+++ book.h
@@ -3,41 +3,43 @@
 
 using namespace std;
 
-class book{
+class book
+{
 private:
-	int ISBN;
+	int isbn;
 	string title;
 	string author;
 	string publisher;
-	int date;
-	int qty;
-	double whole;
-	double cost;
+	int dateAdded;
+	int onHand;
+	double wholesaleCost;
+	double retailCost;
 public:
-	void setISBN(int);
-	int getISBN();
-	void settitle(string);
-	string gettitle();
-	void setauthor(string);
-	string getauthor();
-	void setpublisher(string);
-	string getpublisher();
-	void setdate(int);
-	int getdate();
-	void setqty(int);
-	int getqty();
-	void setcost(double);
-	double getcost();
-	void setwhole(double);
-	double getwhole();
+	void setISBN(int foobar) { isbn = foobar; }
+	int getISBN(){ return isbn; }
 
-	book(int, 
-		string,
-		string,
-		string,
-		int,
-		int,
-		double,
-		double);
-	book();
+	void setTitle(string foobar) { title = foobar; }
+	string getTitle() { return title; }
+
+	void setAuthor(string foobar) { author = foobar; }
+	string getAuthor() { return author; }
+
+	void setPublisher(string foobar) { publisher = foobar; }
+	string getPublisher() { return publisher; }
+
+	void setDateAdded(int foobar) { dateAdded = foobar; }
+	int getDateAdded() { return dateAdded; }
+
+	void setOnHand(int foobar){ onHand = foobar; }
+	int getOnHand() { return onHand; }
+
+	void setWholesaleCost(double foobar) { wholesaleCost = foobar; }
+	double getWholesaleCost() { return wholesaleCost; }
+
+	void setRetailCost(double foobar){ retailCost = foobar; }
+	double getRetailCost() { return retailCost; }
+
+	book(int rISBN, string rTitle, string rAuthor, string rPublisher, int rDate, int rOnHand, double wC, double rC);
+
+	book() { isbn = 0; title = " "; author = " "; publisher = " "; dateAdded = 0; onHand = 0; wholesaleCost = 0; retailCost = 0; }
 };
